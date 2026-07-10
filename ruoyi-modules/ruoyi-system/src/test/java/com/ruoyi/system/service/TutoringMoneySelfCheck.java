59c83bc66963967a0b928d989c09c0e3a9dddc5f
stiupedman <164057484+stiupedman@users.noreply.github.com>
2026-07-10 23:09:06 +0800

    test: 覆盖平台抽成舍入边界

--- ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/service/TutoringMoneySelfCheck.java
+++ ruoyi-modules/ruoyi-system/src/test/java/com/ruoyi/system/service/TutoringMoneySelfCheck.java
@@ -8,6 +8,7 @@ public final class TutoringMoneySelfCheck
     public static void main(String[] args)
     {
         assert TutoringMoney.fee(new BigDecimal("100.00")).compareTo(new BigDecimal("10.00")) == 0;
+        assert TutoringMoney.fee(new BigDecimal("0.05")).compareTo(new BigDecimal("0.01")) == 0;
         assertInvalid(new BigDecimal("0"));
         assertInvalid(new BigDecimal("1.001"));
         assertInvalid(new BigDecimal("100000000"));
